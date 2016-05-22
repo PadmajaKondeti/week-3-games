a8900a272a2decf5055408d5c20df47357194baf
padmajakondeti <padma.kondeti@gmail.com>
2016-05-22 18:00:11 -0400

    hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,71 +2,45 @@
     
     var wins = 0, losses = 0;
     var i=0, guessesLeft = 9, yourGuess="", compGuess="",userGuess ="", newGuess=false, compGuessWord = "";
-    var animalInfo= [
-            {
-                id: 1,
-                name: "Tiger",
-                image: "tiger.jpg"
-            },
-            {
-                id: 2,
-                name: "cow",
-                image: "cow.jpg"
-            },
-            {
-                id: 3,
-                name: "Lion",
-                image: "lion.jpg"
-            },
-            {
-                id: 4,
-                name: "Elephant",
-                image: "elephent.jpg"
-            },
-            {
-                id: 5,
-                name: "Monkey",
-                image: "monkey.jpg"
-            },
-            {
-                id: 6,
-                name: "Panda",
-                image: "panda.jpg"
-            }
-        ];
-    console.log(animalInfo);
+    var animals = ["elephant", "tiger", "cow","monkey", "lion", "panda"];
+    console.log(animals);
     varInitialization();
-    //computer Guesses
-    compGuess = randomString();
-    // iterate over each element in the array
-    for (var i = 0; i < animalInfo.length; i++){
-    // look for the entry with a matching `code` value
-        if (animalInfo[i].id == compGuess){
-        // we found it
-        compGuessWord = animalInfo[i].name;
-        // obj[i].name is the matched result
-        console.log(compGuessWord);
-        }
+    var comGuessAnimal = randomWord();
+    var wordLength = comGuessAnimal.length;
+    var emptyString = " ";
+    console.log(comGuessAnimal);
+    var strComGuessAnimal = comGuessAnimal.split("");
+    console.log(strComGuessAnimal);
+    for (var i = 0; i < wordLength; i++){
+        emptyString = emptyString + "_";
     }
-
-    var animalNum = compGuess;
+    document.getElementById("curWord").innerHTML = emptyString; 
     //guess a letter
     document.getElementById('myText').onkeyup=function(event){   
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-        //yourGuess = yourGuess + userGuess;
-        if (newGuess == true){
-            //computer Guesses
-            compGuess = randomString();
-            newGuess = false;
-        };
-        if (compGuess == userGuess){
-            wins++;
+        yourGuess = yourGuess + userGuess;
+        for (var j = 0; j < wordLength; j++){
+            if (strComGuessAnimal[j] == userGuess){
+                emptyString[j] = strComGuessAnimal[j];
+                matchLetterFound = true;
+                debugger;
+                document.getElementById("curWord").innerHTML = emptyString; 
+                break;
+
+            } else{
+                matchLetterFound = false;
+            }
+        }
+    }
+
+       /* if (compGuess == userGuess){
+            //wins++;
             yourGuess = yourGuess + userGuess;
             yourGuess ="";
-            guessesLeft = 9;
-            compGuess="";
+            //guessesLeft = 9;
+            //compGuess="";
             userGuess ="";
-            i=0;
+            //i=0;
             newGuess=true;
             valAssignment();
            
@@ -90,7 +64,8 @@
                 }
                 valAssignment();              
             } 
-        }        
+        } 
+               
     };
  
     function randomString() {
@@ -100,14 +75,10 @@
         var rnum = Math.floor(Math.random() * chars.length);
         randomstring += chars.substring(rnum,rnum+1);
         return(randomstring);
-    }
+    } */
     function randomWord() {
-        var chars = "12345";
-        var string_length = 1;
-        var randomstring = '';
-        var rnum = Math.floor(Math.random() * chars.length);
-        randomstring += chars.substring(rnum,rnum+1);
-        return(randomstring);
+        var animal = animals[Math.floor(Math.random()*animals.length)];
+        return(animal);
     }
     function varInitialization(){
         // Sets the focus to the input
